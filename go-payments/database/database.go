9fe4b4252c713551b5c269f09b2832270103742f
Dejan Kitic <dejan.kitic@ericsson.com>
2016-04-25 17:30:02 +0100

    Ability to change schema, and command line parsing.

--- go-payments/database/database.go
+++ go-payments/database/database.go
@@ -8,18 +8,28 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+import (
+	"fmt"
+	"log"
+)
+
 //DB is something
 var DB *gorm.DB
 var err error
 
 //InitDB will initilize database connection
-func InitDB() {
-	DB, err = gorm.Open("postgres", "host=10.211.55.33 user=postgres password=mysecretpassword dbname=postgres sslmode=disable")
+func InitDB(dbHostPtr *string, dbHostPortPtr *int, dbUsernamePtr *string, dbPasswordPtr *string, dbNamePtr *string, dbSchemaPtr *string, dbMaxIdleConnectionsPtr *int, dbMaxConnectionsPtr *int) {
+	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", *dbHostPtr, *dbHostPortPtr, *dbUsernamePtr, *dbPasswordPtr, *dbNamePtr)
+	log.Printf("Connection string is: %s", connectionString)
+	DB, err = gorm.Open("postgres", connectionString)
 	if err != nil {
 		panic("failed to connect database")
 	}
-	DB.DB().SetMaxIdleConns(20)
-	DB.DB().SetMaxOpenConns(60)
+	setDbSchemaString := fmt.Sprintf("SET SEARCH_PATH TO \"%s\"", *dbSchemaPtr)
+	log.Printf("%s\n", setDbSchemaString)
+	DB.DB().SetMaxIdleConns(*dbMaxIdleConnectionsPtr)
+	DB.DB().SetMaxOpenConns(*dbMaxConnectionsPtr)
+	DB.Exec(setDbSchemaString)
 	DB.AutoMigrate(&models.ApplicationUser{})
 
 }
--- go-payments/main.go
+++ go-payments/main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"flag"
 	"net/http"
-
+)
+import (
 	"github.com/dekstroza/golang/go-payments/contexts"
 	"github.com/dekstroza/golang/go-payments/database"
+)
+import (
 	"github.com/gocraft/web"
 )
 
 func main() {
-	//runtime.GOMAXPROCS(runtime.NumCPU())
-	database.InitDB()
+	dbHostPtr := flag.String("dbHost", "localhost", "Address or name of the database host.")
+	dbHostPortPtr := flag.Int("dbPort", 5432, "Port on which database server will accept connections.")
+	dbUsernamePtr := flag.String("user", "postgres", "Username for database connection")
+	dbPasswordPtr := flag.String("password", "", "Password for database connection")
+	dbNamePtr := flag.String("db", "postgres", "Name of the database to connect to.")
+	dbSchemaPtr := flag.String("schema", "public", "Name of database schema that will be used.")
+
+	dbMaxIdleConnectionsPtr := flag.Int("maxIdle", 20, "Maximum number of idle connections in the pool.")
+	dbMaxConnectionsPtr := flag.Int("maxConnections", 60, "Maximum number of database connections.")
+
+	flag.Parse()
+	database.InitDB(dbHostPtr, dbHostPortPtr, dbUsernamePtr, dbPasswordPtr, dbNamePtr, dbSchemaPtr, dbMaxIdleConnectionsPtr, dbMaxConnectionsPtr)
 
 	router := web.New(contexts.Context{})
-	//router.Middleware(web.LoggerMiddleware)
-	//router.Middleware(web.ShowErrorsMiddleware)
 	router.Get("/", (*contexts.Context).Root)
 	router.Get("/users", (*contexts.Context).List)
 	router.Get("/users/:id", (*contexts.Context).FindUser)
